ad7544e1b2b3c4bcca2726c574a673bd8a39a16e
LAIA-GitHub <laia.collectiu@gmail.com>
2024-06-05 19:52:00 +0200

    Interaction calling Laia-API

---
+++ Speech-to-Speech.py
@@ -0,0 +1,1 @@
+#This is a Python script to 1. record audio, 2. send Audio to Laia-API, 3. recieve response, 4. decide if catalan or else, 5. generate response audio depending on language, 6. Play Audio
